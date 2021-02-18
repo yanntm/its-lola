164a0da3851ba252326fbe9ab1539be2db74d8e8
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-02-18 19:18:02 +0100

    a complete script that should work + patches for gcc >= 10

--- install_lola.sh
+++ install_lola.sh
@@ -1,5 +1,48 @@
 #! /bin/bash
 
-wget https://github.com/yanntm/extractLola-2019/raw/master/lola.tar.bz2
+
+# grab, this is extracted from 2019 VM
+if [ ! -f lola.tar.bz2 ] ; then
+	wget https://github.com/yanntm/extractLola-2019/raw/master/lola.tar.bz2
+fi
+
+# decompress
+if [ ! -d lola ] ; then
+	tar xvjf lola.tar.bz2
+fi
+
 # todo : automate this part of the script
-# basically : uncompress, install dependencies, make and install sara, make and install lola
+# basically : install dependencies, make and install sara, make and install lola
+mkdir -p usr/
+mkdir -p usr/local/
+mkdir -p usr/local/bin/
+mkdir -p usr/local/share/man/man1/
+
+
+export INSTALL_DIR=$(pwd)/usr/local/
+
+# build
+cd lola/bin
+
+# kimwitu dependency
+cd kimwitu++-2.3.13/
+./configure --prefix=$INSTALL_DIR
+make && make install
+cd ..
+ 
+cd sara/
+./configure --prefix=$INSTALL_DIR
+make && make install
+cd ..
+ 
+cd lola/
+# don't build doc
+cp ../../../patches/Makefile.am .
+# issues with new LTO enabled compilers
+cp ../../../patches/generalized.c ./src/Formula/LTL/
+autoreconf -vfi
+./configure --prefix=$INSTALL_DIR
+make && make install
+cd ..
+
+
